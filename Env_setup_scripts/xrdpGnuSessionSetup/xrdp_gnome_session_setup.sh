95f198be1dc86e26cdbeffaf62f717f747a8d6f2
IndranilNandy <indranil@indranil-VM-2>
2022-06-30 13:48:35 +0530

    Subject: Refactoring code
    Change details:
        1. Remove unnecessary script and move to a function.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/xrdpGnuSessionSetup/xrdp_gnome_session_setup.sh
+++ Env_setup_scripts/xrdpGnuSessionSetup/xrdp_gnome_session_setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+updateXrdpScript() {
+    local replacement=${1}
+    local file=${2}
+
+    sudo grep "$replacement" "$file" >/dev/null && echo "[EXISTS] $replacement: Doing nothing." && return
+
+    sudo sed -i -e "1 s/^$/$replacement/;t" -e "1,// s//$replacement\n/" "$file"
+}
+
+export -f updateXrdpScript
+
 file=${1}
+
 [[ ! -e "$file" ]] && echo -e "Failed to change to full Gnome session.\n$file: File doesn't exist. Exiting." && exit 1
 [[ ! -e "$file".bak ]] && echo "Creating backup file ${file}.bak" && sudo cp "$file" "$file".bak
-(cat ./config/.xrdp-gnome-session-config | xargs -I INPUT ./config/updateXrdpScript.sh INPUT "$file") && echo -e "Successfully changed to full Gnome session.\nReboot needed"
+cat ./config/.xrdp-gnome-session-config | xargs -I INPUT bash -c "updateXrdpScript 'INPUT' $file" && echo -e "Successfully changed to full Gnome session.\nReboot needed"

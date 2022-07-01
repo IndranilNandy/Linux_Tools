31ffbe08d2cdf77a82d808a8b22e3236944eb95b
IndranilNandy <indranil@indranil-VM-2>
2022-07-01 17:02:33 +0530

    Subject: Refactor code
    Change details:
        1. Remove commented out codes.
        2. Introduce .help and .commands files.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/git/configurer/genericDiffAndMergeToolConfigurer/toolsConfigurer.sh
+++ Dev_tools_setup_scripts/git/configurer/genericDiffAndMergeToolConfigurer/toolsConfigurer.sh
@@ -5,16 +5,8 @@ sudo chmod +x $curDir/extMerge
 sudo chmod +x $curDir/extDiff
 sudo chmod +x $curDir/setgittool
 
-# sudo cp $curDir/generic_tool $MYCOMMANDSREPO
-# sudo cp $curDir/extMerge $MYCOMMANDSREPO
 yes | sudo ln -i -s $curDir/extMerge $MYCOMMANDSREPO/extMerge
-
-# sudo cp $curDir/extDiff $MYCOMMANDSREPO
 yes | sudo ln -i -s $curDir/extDiff $MYCOMMANDSREPO/extDiff
-
-# sudo cp $curDir/setgittool $MYCOMMANDSREPO
-# sudo cp -i $curDir/.tools $MYCOMMANDSREPO
-# sudo cp -i $curDir/.commands $MYCOMMANDSREPO
 
 git config --global merge.tool extMerge
 git config --global mergetool.extMerge.cmd 'extMerge "$LOCAL" "$BASE" "$REMOTE" "$MERGED"'

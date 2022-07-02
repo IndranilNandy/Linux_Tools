4704d50238386bfaee2b75442354eb074785a1e1
IndranilNandy <indranil@indranil-VM-2>
2022-07-02 16:41:44 +0530

    Subject: Save a backup of grup and refactor code
    Change details:
        1. Save a copy of grub.
        2. Refactor code.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/display/display_settings_configurer.sh
+++ Env_setup_scripts/display/display_settings_configurer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 pattern='GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"'
 replacement='GRUB_CMDLINE_LINUX_DEFAULT="quiet splash video=hyperv_fb:1920x1080"'
+
+[ ! -e "/etc/default/grub.bak" ] && sudo cp /etc/default/grub /etc/default/grub.bak
+
 sudo sed -i "s/$pattern/$replacement/g" /etc/default/grub
 sudo update-grub
-sudo reboot 
-# echo $pattern $replacement
+# sudo reboot 

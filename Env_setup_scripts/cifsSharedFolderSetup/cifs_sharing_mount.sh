1fa3290935f6b9fef71430208c7644d5902c3e97
IndranilNandy <indranil@indranil-VM-2>
2022-07-02 12:12:57 +0530

    Subject: Add new functionalities and refactor
    Change details:
        1. Add provision for user inputs
        2. Add symbolic links (commands) for mount and unmount
    
    Version: 2.0
    
    References:
    
    Notes:

--- Env_setup_scripts/cifsSharedFolderSetup/cifs_sharing_mount.sh
+++ Env_setup_scripts/cifsSharedFolderSetup/cifs_sharing_mount.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-. ./cifs_sharing_commons.lib
+curDir=$(dirname "$(tracelink "$(which cmount)")")
+(( "$#" == 1 )) && [[ "$1" == "--help" ]] && cat $curDir/help/cmount.help && exit 0
 
+. $curDir/lib/cifs_sharing_commons.lib
+. $curDir/config/config_loader.lib
+
+load_config $*
+
+[[ ! -d $defaultSharedFolder ]] && (mkdir $defaultSharedFolder && echo "Default shared folder created as $defaultSharedFolder")  
 # Mount shared folder
 mount_cifs_sharing
--- Env_setup_scripts/cifsSharedFolderSetup/cifs_sharing_unmount.sh
+++ Env_setup_scripts/cifsSharedFolderSetup/cifs_sharing_unmount.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-. ./cifs_sharing_commons.lib
+curDir=$(dirname "$(tracelink "$(which cmount)")")
+(( "$#" == 1 )) && [[ "$1" == "--help" ]] && cat $curDir/help/cumount.help && exit 0
+
+. $curDir/lib/cifs_sharing_commons.lib
+. $curDir/config/config_loader.lib
+
+load_config $*
 
 # Unmount shared folder
 unmount_cifs_sharing

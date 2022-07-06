1b97abebf9f9008a20e53062bd431885bc0d1414
IndranilNandy <indranil@indranil-VM-2>
2022-07-06 15:21:32 +0530

    Subject: Clean code
    Change details:
        1. Remove commented out codes.
    
    Version:
    
    References:
    
    Notes:

--- Utility_tools_setup_scripts/p4merge/p4merge_installer.sh
+++ Utility_tools_setup_scripts/p4merge/p4merge_installer.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# cd ~/Downloads
-# wget https://cdist2.perforce.com/perforce/r22.1/bin.linux26x86_64/p4v.tgz
-# tar zxvf p4v.tgz
-# sudo mkdir /opt/p4v
-# cd p4v-*
-# sudo mv * /opt/p4v
-# sudo ln -s /opt/p4v/bin/p4merge /usr/local/bin/p4merge
-# sudo nano ~/.gitconfig
-
 [[ -d ./temp ]] || mkdir ./temp
 cd ./temp
 wget https://cdist2.perforce.com/perforce/r22.1/bin.linux26x86_64/p4v.tgz
@@ -19,5 +10,3 @@ sudo mv * /opt/p4v
 sudo ln -s /opt/p4v/bin/p4merge /usr/local/bin/p4merge
 cd ../..
 rm -rf ./temp
-# rm -rf p4v-*
-# rm p4v.tgz
--- Utility_tools_setup_scripts/winmerge/winmerge_installer.sh
+++ Utility_tools_setup_scripts/winmerge/winmerge_installer.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# wget https://github.com/WinMerge/winmerge/releases/download/v2.16.20/WinMerge-2.16.20-x64-Setup.exe
-# wine WinMerge-2.16.20-x64-Setup.exe
-# rm WinMerge-2.16.20-x64-Setup.exe
-
 [[ -d ./temp ]] || mkdir ./temp;
 cd ./temp
 wget https://github.com/WinMerge/winmerge/releases/download/v2.16.20/WinMerge-2.16.20-x64-Setup.exe
 wine WinMerge-2.16.20-x64-Setup.exe
 cd ..
 rm -rf ./temp
-# rm WinMerge-2.16.20-x64-Setup.exe

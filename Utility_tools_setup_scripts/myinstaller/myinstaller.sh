b3b1133454e6f9f740b622003e471e4c7cb03206
IndranilNandy <indranil@indranil-VM-2>
2022-07-06 15:22:52 +0530

    Subject: Add customized installer to install/uninstall all/specific APT packages.
    Change details:
        Customized installer to install/uninstall all/specific APT packages (listed in .localAptPkgTools/.fullAptPkgTools).
        .localAptPkgTools:
            - Lists all the APT packages installed (with myinstaller only) in the current setup/system only.
            - Every new installation of package using minstaller tool will add that package in this list, and uninstallation removes it from this list.
            - Hence, this list shows the current snapshot of the system.
            - '--uninstall --FULL' uses this list.
        .fullAptPkgTools:
            - Lists superset of any APT packages installed across system and time.
            - Any uninstallation doesn't remove any entry from this list.
            - Installation of any new APT package anytime in any system adds new entry in this list, if that was missing.
            - '--install --FULL' option uses this list, and copies each entry to .localAptPkgTools if missing.
    
    Version: 1.0
    
    References:
    
    Notes:

---
+++ Utility_tools_setup_scripts/myinstaller/myinstaller.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+if [[ -z $(which myinstaller) ]]; then
+    curDir=$(pwd)
+else
+    curDir=$(dirname "$(tracelink "$(which myinstaller)")")
+fi
+
+localList=$curDir"/.localAptPkgTools"
+[[ -e $localList ]] || touch $localList
+
+fullList=$curDir"/.fullAptPkgTools"
+
+case ${1} in
+    --install)
+        sudo apt update
+        case ${2} in
+            --FULL)
+                echo "ALL"
+                cat $fullList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X" && ( yes | sudo apt install X ) && ( [[ \$(cat $localList | grep X) ]] || echo X >> $localList ) )" | bash
+                ;;
+            --LOCAL)
+                # echo "ALL"
+                # cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X" && ( yes | sudo apt install X ) )" | bash
+                [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] || \
+                        ( echo "Installing X" && \
+                            ( yes | sudo apt install X  && \
+                                ( [[ \$(cat $localList | grep X) ]] || echo X >> $localList )  && \
+                                ( [[ \$(cat $fullList | grep X) ]] || echo X >> $fullList ) \
+                            ) \
+                        )" \
+                    | bash
+                ;;
+            *)
+                echo ${2}
+                echo "[[ \$(ifinstalled ${2}) ]] || \
+                        ( echo "Installing ${2}" && \
+                            ( yes | sudo apt install ${2}  && \
+                                ( [[ \$(cat $localList | grep ${2}) ]] || echo ${2} >> $localList )  && \
+                                ( [[ \$(cat $fullList | grep ${2}) ]] || echo ${2} >> $fullList ) \
+                            ) \
+                        )" \
+                    | bash
+                ;;
+        esac
+        ;;
+    --uninstall)
+        case ${2} in
+            --FULL)
+                echo "ALL"
+                cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+                ;;
+            --LOCAL)
+                [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+                ;;
+            *)
+                echo ${2}
+                echo "[[ \$(ifinstalled ${2}) ]] && ( echo "Uninstalling ${2}" && ( yes | sudo apt remove ${2} ) && sed -i /^${2}$/d $localList )" | bash
+                ;;
+        esac
+        sudo apt clean
+        yes | sudo apt autoremove
+        ;;
+    --list)
+        cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && echo "Already installed X" " | bash
+        ;;
+    --help)
+        cat $curDir/myinstaller.help
+        ;;
+    *)
+        cat $curDir/myinstaller.help
+        ;;
+esac

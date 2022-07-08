2040ee019093e4539b380e2897143e1deb36ef95
IndranilNandy <57842629+IndranilNandy@users.noreply.github.com>
2022-07-09 00:14:39 +0530

    Add more system configurations + change XRDP script

--- Utility_tools_setup_scripts/xrdp/xrdp_server_installer.sh
+++ Utility_tools_setup_scripts/xrdp/xrdp_server_installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt install xrdp
-sudo systemctl status xrdp
+sudo systemctl enable --now xrdp
+sudo ufw allow from any to any port 3389 proto tcp
+#sudo systemctl status xrdp
 # sudo ufw allow 3389
 # sudo ufw reload
 # reboot

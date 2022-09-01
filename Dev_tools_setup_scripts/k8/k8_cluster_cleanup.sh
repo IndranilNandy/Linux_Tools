7ea3edfe85cc4abc0147e82b5a48738daca1b820
IndranilNandy <indranil@indranil-VM-2>
2022-09-01 20:18:53 +0530

    Subject: [K8] Full tear down of all cluster nodes (partial impl)
    Change details:
        1. [K8] Full tear down of all cluster nodes (partial impl).
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/k8/k8_cluster_cleanup.sh
+++ Dev_tools_setup_scripts/k8/k8_cluster_cleanup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-
-drain_nodes() {
-    echo "Draining nodes"
+drain_node() {
+    echo "Draining node ${1}"
     # TODO: implement later
     # kubectl drain <node name> --delete-emptydir-data --force --ignore-daemonsets
+
+    ! kubectl drain "${1}" --delete-emptydir-data --force --ignore-daemonsets && echo -e "[TEARDOWN] Failed to drain worker node ${1}" && return 1
+
+    # workers=$(cat ./config/.clusterconfig | grep "workers:" | sed "s/workers: *\(.*\)/\1/" | tr " *" "\n" | xargs -I X echo X)
+    # for w in $workers; do
+    #     # sshpass -p "$passwd"
+    #     ! kubectl drain "$w" --delete-emptydir-data --force --ignore-daemonsets && echo -e "[PREREQ] Failed to drain worker nodes" && return 1
+    # done
+    exit 0
 }
 
 reset_tables() {
@@ -12,10 +20,10 @@ reset_tables() {
     sudo ipvsadm -C
 }
 
-delete_nodes() {
-    echo "Deleting nodes"
-    # TODO: implement later
-    # kubectl delete node <node name>
+delete_node() {
+    echo "Deleting node ${1}"
+    ! kubectl delete node "${1}" && echo -e "[TEARDOWN] Failed to delete worker node ${1}" && return 1
+    return 0
 }
 
 clean_CNI_config() {
@@ -24,16 +32,43 @@ clean_CNI_config() {
 }
 
 cleanup() {
-    drain_nodes
+    # drain_nodes
 
-    yes | sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock
+    yes | sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock || echo -e "[TEARDOWN] Failed to reset kubeadm"
     reset_tables
+    # || echo -e "[TEARDOWN] Failed to reset iptables"
 
-    delete_nodes
-    clean_CNI_config || echo -e "[CLEANUP] Failed to clean CNI configuration. This may lead to other issues. You may try to clean /etc/cni/net.d manually"
+    # delete_nodes
+    clean_CNI_config || echo -e "[TEARDOWN] Failed to clean CNI configuration. This may lead to other issues. You may try to clean /etc/cni/net.d manually"
 }
 
 # Cleanup the cluster
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#tear-down
 
+# cleanup
+
+node_c=$(cat ./config/.clusterconfig | grep "controlplane:" | sed "s/controlplane: *\(.*\)/\1/" | tr " *" "\n" | xargs -I X echo X)
+if [[ $(hostname) == "$node_c" ]]; then
+    echo "this is control plane. host: $(hostname)"
+
+    node_ws=$(cat ./config/.clusterconfig | grep "workers:" | sed "s/workers: *\(.*\)/\1/" | tr " *" "\n" | xargs -I X echo X)
+    for w in $node_ws; do
+        echo -e
+        read -p "Want to tear down worker node $w [y] or [n]" ans
+        if [[ $(echo $ans | tr [:upper:] [:lower:]) == "y" ]]; then
+            drain_node "$w" || echo -e "[TEARDOWN] Failed to drain worker node $w. Still proceeding with deleting node"
+            delete node "$w" || echo -e "[TEARDOWN] Failed to delete worker node $w. Still proceeding with kubeadm reset"
+            (./worker-node/worker_tear.sh "$w" || echo -e "[TEARDOWN] Failed to reset kubeadm in worker node $w. Proceeding with next worker node if available.")
+        fi
+    done
+
+    echo -e
+    read -p "Want to tear down the control plane [y] or [n]" ans
+    if [[ $(echo $ans | tr [:upper:] [:lower:]) == "n" ]]; then
+        echo -e "[TEARDOWN] Not tearing down control plane"
+        exit 0
+    fi
+fi
+echo -e "Cleaning up $(hostname)"
 cleanup
+echo "Worker-node: $(hostname)"
---
+++ Dev_tools_setup_scripts/k8/worker-node/worker_tear.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+node="${1}"
+
+echo -e "\nTHIS WILL NOW TEAR DOWN WORKER NODE: $node"
+
+. ./lib/credentials.lib ${1}
+
+! sshpass -p "$passwd" scp -r ~/MyTools/Linux_Tools/Dev_tools_setup_scripts/k8 indranilnandy@"$node":~ && echo -e "[CONTROLPLANE] Failed to copy codebase" && exit 1
+! sshpass -p "$passwd" ssh -t indranilnandy@"$node" "cd ~/k8; bash --login ./k8_cluster_cleanup.sh" && echo -e "[CONTROLPLANE] FAILED!! Setup NOT completed in control plane" && exit 1
+
+exit 0

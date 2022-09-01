977ebdf342497ce5b1ed1d8961e041330c6b9615
IndranilNandy <indranil@indranil-VM-2>
2022-09-01 11:17:37 +0530

    Subject: [K8] Use sshpass to pass password to ssh or scp
    Change details:
        1. Use sshpass to pass password to ssh or scp.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/k8/control-plane/controlplane_setup.sh
+++ Dev_tools_setup_scripts/k8/control-plane/controlplane_setup.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-cnode="${1}"
-# sshpass -p "$x" ssh indranilnandy@k8-controlplanenode ls
-echo -e "SETUP WILL NOW INITIALIZE CONTROL PLANE"
-echo -e "node: $cnode"
 
-! scp -r ~/MyTools/Linux_Tools/Dev_tools_setup_scripts/k8 indranilnandy@"$cnode":~ && echo -e "[CONTROLPLANE] Failed to copy codebase" && exit 1
-! ssh -t indranilnandy@"$cnode" "cd ~/k8; bash --login ./k8_cluster_init.sh" --node=control --cni=calico && echo -e "[CONTROLPLANE] FAILED!! Setup NOT completed in control plane" && exit 1
+cnode="${1}"
+
+echo -e "\nSETUP WILL NOW INITIALIZE CONTROL PLANE: $cnode"
+
+. ./lib/credentials.lib ${1}
+# sshpass -p "$passwd" ssh indranilnandy@"$cnode" ls || exit 1
+
+! sshpass -p "$passwd" scp -r ~/MyTools/Linux_Tools/Dev_tools_setup_scripts/k8 indranilnandy@"$cnode":~ && echo -e "[CONTROLPLANE] Failed to copy codebase" && exit 1
+! sshpass -p "$passwd" ssh -t indranilnandy@"$cnode" "cd ~/k8; bash --login ./k8_cluster_init.sh" --node=control --cni=calico && echo -e "[CONTROLPLANE] FAILED!! Setup NOT completed in control plane" && exit 1
 
 echo -e "[CONTROLPLANE] Setup completed in control plane. Now fetching token and ca-cert-hash"
 mkdir ./credentials
-! ssh -T indranilnandy@"$cnode" "kubeadm token list | cut -f1 -d' ' | tail -n1" >./credentials/token && echo -e "[CONTROLPLANE] FAILED!! Missing token, probable expired, you need to recreate. Use command 'kubeadm token create'" && exit 1
-! ssh -T indranilnandy@"$cnode" "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
+! sshpass -p "$passwd" ssh -T indranilnandy@"$cnode" "kubeadm token list | cut -f1 -d' ' | tail -n1" >./credentials/token && echo -e "[CONTROLPLANE] FAILED!! Missing token, probable expired, you need to recreate. Use command 'kubeadm token create'" && exit 1
+! sshpass -p "$passwd" ssh -T indranilnandy@"$cnode" "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
    openssl dgst -sha256 -hex | sed 's/^.* //'" >./credentials/ca-cert-hash && echo -e "[CONTROLPLANE] FAILED!! ca-cert-hash NOT available" && exit 1
 
 exit 0
--- Dev_tools_setup_scripts/k8/worker-node/join_cluster.sh
+++ Dev_tools_setup_scripts/k8/worker-node/join_cluster.sh
@@ -2,4 +2,5 @@
 
 ! sudo kubeadm join --token $(cat ./credentials/token) k8-controlplanenode:6443 --discovery-token-ca-cert-hash sha256:$(cat ./credentials/ca-cert-hash) --cri-socket=unix:///var/run/cri-dockerd.sock && echo -e "[WORKER] FAILED!! Not able to join to the control plane" && exit 1
 rm -rf ./credentials
+
 exit 0

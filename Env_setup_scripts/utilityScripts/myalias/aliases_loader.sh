df5ddfa717d2bf509d1bf371c20bb4b08b2e62a6
IndranilNandy <indranilnandy@ubuntudevbox>
2022-09-17 18:38:03 +0530

    Subject: [myalias] Moving myalias from configuration to utilityScripts
    Change details:
        1. Moving myalias from configuration to utilityScripts.
    
    Version:
    
    References:
    
    Notes:

---
+++ Env_setup_scripts/utilityScripts/myalias/aliases_loader.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ -z $(which myalias) ]]; then
+    curDir=$(pwd)
+else
+    curDir=$(dirname "$(tracelink "$(which myalias)")")
+fi
+
+list=$(ls -a "$curDir"/.aliases | grep -E "\..*aliases$" | xargs -I X cat "$curDir"/.aliases/X | grep -E -v "^$|#.*")
+
+mapfile -t CommandsList <<<"$list"
+for item in "${CommandsList[@]}"; do
+    # echo "alias ${item}"
+    alias "${item}"
+done

1de835117b8d490dd66fefef767683d87c13c622
IndranilNandy <indranilnandy@ubuntudevbox>
2022-09-20 13:23:16 +0530

    Subject: [myfunc] More changes
    Change details:
        1. More changes.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/utilityScripts/myfunc/myfunc_loader.sh
+++ Env_setup_scripts/utilityScripts/myfunc/myfunc_loader.sh
@@ -6,23 +6,6 @@ else
     curDir=$(dirname "$(tracelink "$(which myfunc)")")
 fi
 
-# list=$(ls -a "$curDir"/.aliases | grep -E "\..*aliases$" | xargs -I X cat "$curDir"/.aliases/X | grep -E -v "^$|#.*")
-
-# mapfile -t CommandsList <<<"$list"
-# for item in "${CommandsList[@]}"; do
-#     alias=$(echo "${item}" | sed "s/ *\([^=]*\)=.*/\1/")
-#     command=$(echo "${item}" | sed "s/[^=]*= *\(.*\)/\1/")
-#     # echo -e "alias=$alias command=$command"
-    
-#     if [[ -z $(which ${alias}) ]]; then
-#         alias "${alias}=echo ${alias}=${command}; ${command}"
-#     else
-#         echo -e "[ERROR] Cannot set alias ${item}. $(which ${alias}) already exists"
-#     fi
-# done
-
-# functest() {
-#     echo "Hi this is a test function"
-# }
-
-. "$curDir"/.funcs/.genericfunctions
+for funcdef in $(echo "$curDir"/.funcs/**/.* | xargs -n1 echo | grep -E "\.function$"); do
+    source "$funcdef"
+done

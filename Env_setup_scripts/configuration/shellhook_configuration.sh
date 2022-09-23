81b9ca869f131e8f425a9386bb82be7cc3882851
IndranilNandy <indranilnandy@ubuntudevbox>
2022-09-23 19:41:51 +0530

    Subject: [myhistory] Add myhistory utility to show executing directory along with the executed command in history
    Change details:
        1. Add myhistory utility to show executing directory along with the executed command in history.
    
    Version:
    
    References:
    
    Notes:

---
+++ Env_setup_scripts/configuration/shellhook_configuration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[[ -f $MYCONFIGLOADER/.bash-preexec.sh ]] && echo -e "[shellhook] Alread exists" && exit 0
+
+# Pull down our file from GitHub and write it to your home directory as a hidden file.
+curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o "$MYCONFIGLOADER"/.bash-preexec.sh
+# Source our file at the end of our bash profile (e.g. ~/.bashrc, ~/.profile, or ~/.bash_profile)
+[[ $(cat ~/.bashrc | grep -E -v " *#" | grep "$MYCONFIGLOADER/.bash-preexec.sh") ]] || echo "[[ -f $MYCONFIGLOADER/.bash-preexec.sh ]] && source $MYCONFIGLOADER/.bash-preexec.sh" >> ~/.bashrc

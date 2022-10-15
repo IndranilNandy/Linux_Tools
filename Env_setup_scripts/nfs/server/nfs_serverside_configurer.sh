b7b8abb174237e58831812ae10a6e9f8350a86ae
IndranilNandy <indranilnandy@ubuntudevbox>
2022-10-15 13:50:15 +0530

    Subject:[mynfs] Issue: K8 PV recycler pods can't recycle PV having NFS share mounted
    Change details:
        1. Make the NFS share with no owner and everyone access to modify.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/nfs/server/nfs_serverside_configurer.sh
+++ Env_setup_scripts/nfs/server/nfs_serverside_configurer.sh
@@ -12,17 +12,31 @@ create_backup() {
     [[ -f /etc/exports-backup ]] || sudo cp /etc/exports /etc/exports-backup
 }
 
+# Needed for PV recycler pod in K8 to have access to clean PV
+none_owner() {
+    xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chown nobody:nogroup X" | bash
+}
+
+# Needed for PV recycler pod in K8 to have access to clean PV
+everyone_can_modify() {
+    xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chmod 777 X" | bash
+}
+
 # https://ubuntu.com/server/docs/service-nfs
 config_server_setup() {
     create_backup
 
     cat ../config/server/.commons | update_etc_exports
     cat ../config/server/.commons | create_non_existing_shares
+    cat ../config/server/.commons | none_owner
+    cat ../config/server/.commons | everyone_can_modify
     
     host_specific_exportsfile=$(hostname)-$(hostname -I | awk '{ print $1 }')
 
     cat ../config/server/"$host_specific_exportsfile" | update_etc_exports
     cat ../config/server/"$host_specific_exportsfile" | create_non_existing_shares
+    cat ../config/server/"$host_specific_exportsfile" | none_owner
+    cat ../config/server/"$host_specific_exportsfile" | everyone_can_modify
 
     sudo exportfs -a
     sudo systemctl restart nfs-kernel-server.service

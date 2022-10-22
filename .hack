66b5a0007e8788e70b728d13f6ff4071121955ba
IndranilNandy <indranilnandy@ubuntudevbox>
2022-10-22 19:35:23 +0530

    Subject:[myinstaller] Add support for snap package manager
    Change details:
        1. Add support for snap package manager.
    
    Version:
    
    References:
    
    Notes:

--- .hack
+++ .hack
@@ -8,6 +8,7 @@ echo "arg=$arg"
        --custom-setup=*)
         path=$(echo $arg | sed "s/--custom-setup=\(.*\)/\1/")
         echo "path=$path"
+        # TODO: Add support for passing snap tools list
         [[ -z "$path" ]] || yes | sudo ln -s -i $(pwd)/Custom_setup_definitions/$path/.$path-aptpkgtools $(pwd)/Utility_tools_setup_scripts/myinstaller/.fullAptPkgTools
         ;;
 
--- Utility_tools_setup_scripts/myinstaller/myinstaller.sh
+++ Utility_tools_setup_scripts/myinstaller/myinstaller.sh
@@ -9,33 +9,52 @@ fi
 localList=$curDir"/.localAptPkgTools"
 [[ -e $localList ]] || touch $localList
 
+localSnapList=$curDir"/.localSnapPkgTools"
+[[ -e $localSnapList ]] || touch $localSnapList
+
 fullList=$curDir"/.fullAptPkgTools"
+fullSnapList=$curDir"/fullSnapPkgTools"
 export curDir
+
+tool="apt"
 
 case ${1} in
 --install)
     sudo apt update
     case ${2} in
     --FULL)
-        cat $fullList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X" && ( yes | sudo apt install X ) && ( [[ \$(cat $localList | grep X) ]] || echo X >> $localList ) )" | bash
+        cat $fullList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X using APT" && ( yes | sudo apt install X ) && ( [[ \$(cat $localList | grep X) ]] || echo X >> $localList ) )" | bash
+        cat $fullSnapList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X using SNAP" && ( yes | sudo snap install X ) && ( [[ \$(cat $localSnapList | grep X) ]] || echo X >> $localSnapList ) )" | bash
         ;;
     --LOCAL)
         # cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] || ( echo "Installing X" && ( yes | sudo apt install X ) )" | bash
         [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] || \
-                        ( echo "Installing X" && \
+                        ( echo "Installing X using APT" && \
                             ( yes | sudo apt install X  && \
                                 ( [[ \$(cat $localList | grep X) ]] || echo X >> $localList )  && \
                                 ( [[ \$(cat $fullList | grep X) ]] || echo X >> $fullList ) \
+                            ) \
+                        ) || \
+                        ( echo "Installing X using SNAP" && \
+                            ( yes | sudo snap install X  && \
+                                ( [[ \$(cat $localSnapList | grep X) ]] || echo X >> $localSnapList )  && \
+                                ( [[ \$(cat $fullSnapList | grep X) ]] || echo X >> $fullSnapList ) \
                             ) \
                         )" |
             bash
         ;;
     *)
         echo "[[ \$(ifinstalled ${2}) ]] || \
-                        ( echo "Installing ${2}" && \
+                        ( echo "Installing ${2} using APT" && \
                             ( yes | sudo apt install ${2}  && \
                                 ( [[ \$(cat $localList | grep ${2}) ]] || echo ${2} >> $localList )  && \
                                 ( [[ \$(cat $fullList | grep ${2}) ]] || echo ${2} >> $fullList ) \
+                            ) \
+                        ) || \
+                        ( echo "Installing ${2} using SNAP" && \
+                            ( yes | sudo snap install ${2}  && \
+                                ( [[ \$(cat $localSnapList | grep ${2}) ]] || echo ${2} >> $localSnapList )  && \
+                                ( [[ \$(cat $fullSnapList | grep ${2}) ]] || echo ${2} >> $fullSnapList ) \
                             ) \
                         )" |
             bash
@@ -46,13 +65,16 @@ case ${1} in
 --uninstall)
     case ${2} in
     --FULL)
-        cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+        cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X using APT" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+        cat $localSnapList | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X using SNAP" && ( yes | sudo snap remove X ) && sed -i /^X$/d $localSnapList )" | bash
         ;;
     --LOCAL)
-        [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+        [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X using APT" && ( yes | sudo apt remove X ) && sed -i /^X$/d $localList )" | bash
+        [[ ${3} ]] && cat ${3} | xargs -I X echo "[[ \$(ifinstalled X) ]] && ( echo "Uninstalling X using SNAP" && ( yes | sudo snap remove X ) && sed -i /^X$/d $localSnapList )" | bash
         ;;
     *)
-        echo "[[ \$(ifinstalled ${2}) ]] && ( echo "Uninstalling ${2}" && ( yes | sudo apt remove ${2} ) && sed -i /^${2}$/d $localList )" | bash
+        echo "[[ \$(ifinstalled ${2}) ]] && ( echo "Uninstalling ${2} using APT" && ( yes | sudo apt remove ${2} ) && sed -i /^${2}$/d $localList )" | bash
+        echo "[[ \$(ifinstalled ${2}) ]] && ( echo "Uninstalling ${2} using SNAP" && ( yes | sudo snap remove ${2} ) && sed -i /^${2}$/d $localSnapList )" | bash
         ;;
     esac
     sudo apt clean
@@ -60,7 +82,8 @@ case ${1} in
     xargs -I X echo "grep -q X  \"\$curDir\"/../../_verify/.allTools || echo \"X:X\" >> \"\$curDir\"/../../_verify/.allTools" <"$localList" | bash
     ;;
 --list)
-    cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && echo "Already installed X" " | bash
+    cat $localList | xargs -I X echo "[[ \$(ifinstalled X) ]] && echo "APT- Already installed X" " | bash
+    cat $localSnapList | xargs -I X echo "[[ \$(ifinstalled X) ]] && echo "SNAP- Already installed X" " | bash
     ;;
 --help)
     cat $curDir/myinstaller.help
@@ -71,3 +94,4 @@ case ${1} in
 esac
 
 xargs -I X echo "grep -q X  $curDir/../../_verify/.allTools || ( echo X >> $localList && echo \"X:X\" >> $curDir/../../_verify/.allTools )" < $fullList | bash
+xargs -I X echo "grep -q X  $curDir/../../_verify/.allTools || ( echo X >> $localSnapList && echo \"X:X\" >> $curDir/../../_verify/.allTools )" < $fullSnapList | bash

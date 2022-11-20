58cc1f602b69206335a4f3eca1950ce9f2a5f035
IndranilNandy <indranilnandy@ubuntudevbox>
2022-11-20 19:35:26 +0530

    Subject: [ws_setup] Change config file format
    Change details:
        1. Change config file format.
    
    Version:
    
    References:
    
    Notes:

--- Post_setup/ws_setup/workspace_clone.sh
+++ Post_setup/ws_setup/workspace_clone.sh
@@ -8,14 +8,15 @@ fi
 
 . "$curDir"/../../.systemConfig
 
-repoConfig="$curDir"/config/.allRepoConfig
 tempFile="$curDir"/tmp
+wsConfig="$curDir"/config/.wsConfig
 
-grep -v "^ *#" < "$repoConfig" | while IFS= read -r line
+grep -v "^ *#" < "$wsConfig" | while IFS= read -r line
 do
-    remoterepo=$(echo "$line" | sed 's/\(.*\)#.*#.*/\1/')
-    branch=$(echo "$line" | sed 's/.*#\(.*\)#.*/\1/')
-    localrepo=$(echo "$line" | sed 's/.*#.*#\(.*\)/\1/')
+    remoterepo=$(echo "$line" | awk '{ print $1 }');
+    branch=$(echo "$line" | awk '{ print $2 }');
+    localrepo=$(echo "$line" | awk '{ print $3 }');
+    tags=$(echo "$line" | awk '{ print $4 }');
 
     echo -e "\nRepo: $remoterepo\nBranch: $branch\nWorkspace: $user_devroot/$repo_root/$localrepo\n"
     git clone -b "$branch" "$remoterepo" "$user_devroot"/"$repo_root"/"$localrepo" 2>"$tempFile" || ([[ $(cat $tempFile | grep 'already exists') ]] && echo "Repo already exists" || echo "Error: $(cat $tempFile)")

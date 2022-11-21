8c1b36e9b2e1ae0472811f96fb90112cc1bb1145
IndranilNandy <indranilnandy@ubuntudevbox>
2022-11-21 21:31:08 +0530

    Subject: [qcd] Update mappings
    Change details:
        1. Update mappings.
    
    Version:
    
    References:
    
    Notes:

--- Post_setup/ws_setup/workspace_clone.sh
+++ Post_setup/ws_setup/workspace_clone.sh
@@ -11,17 +11,17 @@ fi
 tempFile="$curDir"/tmp
 wsConfig="$curDir"/config/.wsConfig
 
-add_quicklink() {
-    dir_alias=${1}
-    dir_target=${2}
-    echo "alias=$dir_alias target=$dir_target which=$(which myfunc)"
+# add_quicklink() {
+#     dir_alias=${1}
+#     dir_target=${2}
+#     echo "alias=$dir_alias target=$dir_target which=$(which myfunc)"
 
-    instl_script=" source ~/.bashrc; \
-echo \"Installing version: $dir_alias\"; \
-exit 0 "
+#     instl_script=" source ~/.bashrc; \
+# echo \"Installing version: $dir_alias\"; \
+# exit 0 "
 
-    bash --init-file <(echo "$instl_script;")
-}
+#     bash --init-file <(echo "$instl_script;")
+# }
 
 grep -v "^ *#" <"$wsConfig" | while IFS= read -r line; do
     remoterepo=$(echo "$line" | awk '{ print $1 }')
@@ -33,9 +33,9 @@ grep -v "^ *#" <"$wsConfig" | while IFS= read -r line; do
 
     echo -e "\nRepo: $remoterepo\nBranch: $branch\nWorkspace: $user_devroot/$repo_root/$localrepo\nShould download?: $(echo "$shouldDwld" | tr [:upper:] [:lower:])\nTags: $tags\nQuicklink: $qlink\n"
     [ $(echo "$shouldDwld" | tr [:upper:] [:lower:]) == "n" ] || git clone -b "$branch" "$remoterepo" "$user_devroot"/"$repo_root"/"$localrepo" 2>"$tempFile" || ([[ $(cat $tempFile | grep 'already exists') ]] && echo "Repo already exists" || echo "Error: $(cat $tempFile)")
-    [ -z "$qlink" ] || echo "$qlink" "$user_devroot"/"$repo_root"/"$localrepo"
+    # [ -z "$qlink" ] || echo "$qlink" "$user_devroot"/"$repo_root"/"$localrepo"
 
-    [ -z "$qlink" ] || add_quicklink "$qlink" "$user_devroot"/"$repo_root"/"$localrepo"
+    # [ -z "$qlink" ] || add_quicklink "$qlink" "$user_devroot"/"$repo_root"/"$localrepo"
 
 done
 

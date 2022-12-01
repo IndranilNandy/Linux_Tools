19652972b8a841109e3180560261e3c9f6a0e2bc
IndranilNandy <indranilnandy@ubuntudevbox>
2022-12-01 10:42:33 +0530

    Subject: [mycrontab] Define a envloader script for all those jobs which require access to bash functions
    Change details:
        1. Define a envloader script for all those jobs which require access to bash functions.
        2. Jobs need to source this file at the start.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/utilityScripts/mycrontab/cronjobs/sdkman_update.sh
+++ Env_setup_scripts/utilityScripts/mycrontab/cronjobs/sdkman_update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+# Source this loader file for any job which requires access to bash functions defined in .bashrc.
+# Since we cannot directly include .bashrc, as it has a check for non-interactive shell. Either we have to disable that check,
+# or put our required functionality explicitly in another script and source that scritpt. This is what we are doing here.
+
+source $(dirname "$BASH_SOURCE")/jobhelpers/envloader.sh
+
 echo "Running script" | ts '[%Y-%m-%d %H:%M:%S]'
-
-# source /home/indranilnandy/.bashrc
-
-[[ -e /home/indranilnandy/.myconfig/.configloader/.envloader ]] && source /home/indranilnandy/.myconfig/.configloader/.envloader
-export SDKMAN_DIR="/home/indranilnandy/.sdkman"
-[[ -s "/home/indranilnandy/.sdkman/bin/sdkman-init.sh" ]] && source "/home/indranilnandy/.sdkman/bin/sdkman-init.sh"
 
 sdk update
 echo "Completed." | ts '[%Y-%m-%d %H:%M:%S]'

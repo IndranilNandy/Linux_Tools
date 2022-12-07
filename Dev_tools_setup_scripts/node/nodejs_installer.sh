800ede305238f5065bb0838a0a5cc0d0e5b972dc
IndranilNandy <indranilnandy@ubuntudevbox>
2022-12-07 11:45:39 +0530

    Subject: [node] Update comments
    Change details:
        1. Update comments.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/node/nodejs_installer.sh
+++ Dev_tools_setup_scripts/node/nodejs_installer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://nodejs.org/en/download/package-manager/#n
+# https://phoenixnap.com/kb/update-node-js-version
 yes | sudo apt install npm
 sudo npm install -g n
 sudo n lts

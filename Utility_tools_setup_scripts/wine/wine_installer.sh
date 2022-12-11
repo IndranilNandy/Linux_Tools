8c6c6698a0cb1621dda49ef81a52aa2f316bf5a4
IndranilNandy <indranilnandy@testbox1>
2022-12-11 12:31:53 +0530

    Subject: [wine] Fixed issue with installation on Jammy release
    Change details:
        1. Fixed issue with installation on Jammy release.
    
    Version:
    
    References:
    
    Notes:

--- Utility_tools_setup_scripts/wine/wine_installer.sh
+++ Utility_tools_setup_scripts/wine/wine_installer.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
+# https://wiki.winehq.org/Ubuntu
+
 check_architecture() {
     echo "Running check_architecture"
     [ $(dpkg --print-architecture) = "amd64" ] || exit 1
-    [ $(dpkg --print-foreign-architectures) = "i386" ] || ( yes | sudo dpkg --add-architecture i386 )
+    [ $(dpkg --print-foreign-architectures) = "i386" ] || (yes | sudo dpkg --add-architecture i386)
     [ $(dpkg --print-foreign-architectures) = "i386" ] || exit 1
 }
 
 add_winehq_repo() {
     echo "Running add_winehq_repo"
-    wget -nc https://dl.winehq.org/wine-builds/winehq.key
-    sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
-    wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-    sudo mv winehq-jammy.sources /etc/apt/sources.list.d/
+    # wget -nc https://dl.winehq.org/wine-builds/winehq.key
+    # sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
+    # wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+    # sudo mv winehq-jammy.sources /etc/apt/sources.list.d/
+
+    # Commented out the above implementation as it stopped working for jammy release
+    sudo mkdir -pm755 /etc/apt/keyrings
+    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 }
 
 install_wine() {
     echo "Running install_wine"
-    # sudo apt update && ( yes | sudo apt install --install-recommends winehq-stable ) << this didn't work
-    sudo apt update && ( yes | sudo apt install wine )
+    # sudo apt update && ( yes | sudo apt install wine )
+    sudo apt update && (yes | sudo apt install --install-recommends winehq-stable)
 }
 
 verify_installation() {
     echo "Running verify_installation"
-    wine --version || ( echo "Installation failed" && exit 1 )
+    wine --version || (echo "Installation failed" && exit 1)
 }
 
-( check_architecture ) && (add_winehq_repo ) && ( install_wine ) && ( verify_installation )
+(check_architecture) && (add_winehq_repo) && (install_wine) && (verify_installation)
 
 # Fix misplaced launcher
-sudo ln -s /usr/share/doc/wine/examples/wine.desktop /usr/share/applications/ 
+sudo ln -s /usr/share/doc/wine/examples/wine.desktop /usr/share/applications/
 ./configurer/wine_configurer.sh
 ./gecko/gecko_installer.sh
--- Utility_tools_setup_scripts/wine/wine_uninstaller.sh
+++ Utility_tools_setup_scripts/wine/wine_uninstaller.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-yes | sudo apt purge wine
+# yes | sudo apt purge wine
+yes | sudo apt purge winehq-stable
 sudo apt clean
 yes | sudo apt autoremove

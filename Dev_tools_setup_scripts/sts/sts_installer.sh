afb9b8440683fd17e7ce373ad202342edf2ca2f3
IndranilNandy <indranilnandy@ubuntudevbox>
2023-01-18 23:30:05 +0530

    Subject: [sts] Change STS installation location and add to favourites
    Change details:
        1. Change STS installation location and add to favourites.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/sts/sts_installer.sh
+++ Dev_tools_setup_scripts/sts/sts_installer.sh
@@ -6,17 +6,17 @@ installer() {
         cd tmp || return
 
         wget -O sts.tar.gz "https://download.springsource.com/release/STS4/4.16.1.RELEASE/dist/e4.25/spring-tool-suite-4-4.16.1.RELEASE-e4.25.0-linux.gtk.x86_64.tar.gz"
-        mkdir -p "$HOME"/sts
-        tar xvzf sts.tar.gz -C "$HOME"/sts
+        sudo mkdir -p /opt/sts
+        sudo tar xvf sts.tar.gz --one-top-level=/opt/sts --strip-components 1
     )
     rm -rf tmp
 }
 
 add_env_var() {
     envloader="$MYCONFIGLOADER"/.envloader
-    stshome="$HOME"/sts
+    stshome=/opt
 
-    stspath="$stshome"/"sts-4.16.1.RELEASE"
+    stspath="$stshome"/sts
     expPath="export PATH=$PATH"
 
     yes | sudo ln -s -i "$stspath"/SpringToolSuite4 "$MYCOMMANDSREPO"/sts
@@ -24,13 +24,15 @@ add_env_var() {
     (echo "$expPath" | grep -E -v " *#" | grep -q "$stspath") && return 0
     expPath="$expPath":"$stspath"
 
-    if grep -v " *#" "$envloader" | grep -q -E "export PATH" ; then
+    if grep -v " *#" "$envloader" | grep -q -E "export PATH"; then
         sed -i "s#\(export PATH=.*\)#$expPath#" "$envloader"
     else
         echo "$expPath" >>"$envloader"
     fi
 }
 
-installer
-add_env_var
+validate_and_install() {
+    desktop-file-validate sts.desktop && sudo desktop-file-install sts.desktop
+}
 
+(installer && add_env_var && validate_and_install) || echo -e "Error in installing Spring Tool Suite"
--- Dev_tools_setup_scripts/sts/sts_uninstaller.sh
+++ Dev_tools_setup_scripts/sts/sts_uninstaller.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
-
 remove_env_vars() {
     envloader="$MYCONFIGLOADER"/.envloader
-    stshome="$HOME"/sts
-    stspath="$stshome"/"sts-4.16.1.RELEASE"
+    stshome=/opt
+    stspath="$stshome"/sts
 
     sed -i "s@\(.*\):$stspath\(.*\)@\1\2@" "$envloader"
 }
@@ -15,7 +13,7 @@ remove_symlink() {
 }
 
 remove_binary() {
-    rm -rf "$HOME"/sts
+    rm -rf /opt/sts
 }
 
 remove_env_vars && remove_symlink && remove_binary || echo -e "[sts] Failed to uninstall"

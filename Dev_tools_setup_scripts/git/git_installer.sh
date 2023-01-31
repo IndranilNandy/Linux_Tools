9803329f0cc5c1ff23b07e5f0518c2fd48795066
IndranilNandy <indranilnandy@indranilnandy-testvm-1>
2023-01-31 20:01:58 +0530

    Subject: [git] Fix issue with softclean
    Change details:
        1. Fix issue with softclean.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/git/git_installer.sh
+++ Dev_tools_setup_scripts/git/git_installer.sh
@@ -3,4 +3,5 @@
 [[ -n $(which git) ]] || (sudo apt update && (yes | sudo apt install git))
 
 ./configurer/git_configurer.sh
-ifinstalled gcmcore || ./gcm_installer.sh
+# ifinstalled gcmcore || ./gcm_installer.sh
+dpkg-query -l gcm > /dev/null && echo -e "GCM is already installed and configured" || ./gcm_installer.sh

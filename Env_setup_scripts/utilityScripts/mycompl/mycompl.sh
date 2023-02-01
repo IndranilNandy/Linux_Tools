5097d59084120870672a2b180f88d8e6a4e247ee
IndranilNandy <indranilnandy@indranilnandy-testvm-1>
2023-02-01 19:04:51 +0530

    Subject: [mycompl] Add option for --dynamic and --temp
    Change details:
        1. Add option for --dynamic and --temp.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/utilityScripts/mycompl/mycompl.sh
+++ Env_setup_scripts/utilityScripts/mycompl/mycompl.sh
@@ -10,6 +10,20 @@ help() {
     cat "$curDir"/help/mycompl.help
 }
 
+addCompletions() {
+    local scriptName="${1}"
+    local compl_entry="${@:2}"
+
+    [[ $(cat "$curDir"/.completions/"$scriptName" | grep -v "^#" | grep -- "$compl_entry") ]] && return 1
+
+    p=$(echo "$compl_entry" | awk '{ print $NF }')
+    # Prune outdated existing entry
+    [[ $(cat "$curDir"/.completions/"$scriptName" | grep -v "^#" | grep -- " $p$") ]] && sed -i "/ $p$/d" "$curDir"/.completions/"$scriptName"
+
+    echo "$compl_entry" >>"$curDir"/.completions/"$scriptName"
+    return 0
+}
+
 case ${1} in
 --config)
     ls -a "$curDir"/.completions | grep -E "\..*completions$" | xargs -I X echo "echo \"echo Opening $curDir/.completions/X && editor -w $curDir/.completions/X\" | bash" | bash
@@ -20,13 +34,13 @@ case ${1} in
 '')
     help
     ;;
+--temp)
+    addCompletions ".tempcompletions" "${@:2}" || exit 0
+    ;;
+--dynamic)
+    addCompletions ".dynamiccompletions" "${@:2}" || exit 0
+    ;;
 *)
-    [[ $(cat "$curDir"/.completions/.genericcompletions | grep -v "^#" | grep -- "$*" ) ]] && exit 0
-
-    p=$(echo "$*" | awk '{ print $NF }')
-    # Prune outdated existing entry
-    [[ $(cat "$curDir"/.completions/.genericcompletions | grep -v "^#" | grep -- " $p$" ) ]] && sed -i "/ $p$/d" "$curDir"/.completions/.genericcompletions
-    
-    echo "$*" >> "$curDir"/.completions/.genericcompletions
+    addCompletions ".scriptgeneratedcompletions" "$@" || exit 0
     ;;
 esac

76c8d245e950cb941a6d9f1dd48b32a2e8c7a25f
IndranilNandy <indranilnandy@vm-devbox1>
2023-03-09 17:33:22 +0530

    Subject: [androidstudio] Download system images (avd)
    Change details:
        1. Download system images (avd).
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/androidstudio/installers/sdkmanager_install_packages.sh
+++ Dev_tools_setup_scripts/androidstudio/installers/sdkmanager_install_packages.sh
@@ -9,12 +9,22 @@ install_packages() {
     # To see those packages, run 'sdkmanager --list_installed'
     # Here, we are downloading them explicitly, since Flutter installation requires these packages before we manually open android studio for the first time.
     # https://developer.android.com/studio/intro/update#recommended
-    packages=('build-tools;33.0.1' 'emulator' 'patcher;v4' 'platform-tools' 'platforms;android-33-ext4')
+    local packages=('build-tools;33.0.1' 'emulator' 'patcher;v4' 'platform-tools' 'platforms;android-33-ext4')
     for package in "${packages[@]}"; do
         echo "\"$package\""
         yes | "$ANDROID_SDK"/cmdline-tools/latest/bin/sdkmanager --install "$package"
     done
     # "$ANDROID_SDK"/cmdline-tools/latest/bin/sdkmanager --install
+}
+
+install_system_images() {
+    # Note: Downloading Android AVD system images. We'll need these later for Flutter development
+    # Ref. https://stackoverflow.com/questions/56090813/i-can-get-any-emulators-running-in-vs-code
+    local system_images=('system-images;android-27;google_apis_playstore;x86' 'system-images;android-30;google_apis_playstore;x86' 'system-images;android-33-ext4;google_apis_playstore;x86_64')
+    for image in "${system_images[@]}"; do
+        echo "\"$image\""
+        yes | "$ANDROID_SDK"/cmdline-tools/latest/bin/sdkmanager --install "$image"
+    done
 }
 
 update_packages() {
@@ -25,4 +35,4 @@ accept_licenses() {
     yes | "$ANDROID_SDK"/cmdline-tools/latest/bin/sdkmanager --licenses
 }
 
-install_packages && update_packages && accept_licenses
+install_packages && install_system_images && update_packages && accept_licenses

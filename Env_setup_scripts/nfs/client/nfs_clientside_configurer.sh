0122b73862e6b648a9a24dcc4eeef9ee595aa6a9
IndranilNandy <indranilnandy@vm-devbox1>
2023-04-18 11:14:49 +0530

    Subject: [mynfs] Add provision for commenting in config files + change default configuration
    Change details:
        1. Add provision for commenting in config files + change default configuration.
    
    Version:
    
    References:
    
    Notes:

--- Env_setup_scripts/nfs/client/nfs_clientside_configurer.sh
+++ Env_setup_scripts/nfs/client/nfs_clientside_configurer.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 create_non_existing_mounts() {
-    xargs -I X echo X | awk '{ print $2 }' | xargs -I X echo "[[ -d X ]] || sudo mkdir -p X" | bash
+    grep -E -v " *#" | xargs -I X echo X | awk '{ print $2 }' | xargs -I X echo "[[ -d X ]] || sudo mkdir -p X" | bash
 }
 
 mount_all_shares() {
-    xargs -I X echo X | awk '{ print "sudo mount "$1" "$2  }' | bash
+    grep -E -v " *#" | xargs -I X echo X | awk '{ print "sudo mount "$1" "$2  }' | bash
 }
 
 # https://ubuntu.com/server/docs/service-nfs
 config_client_setup() {
     cat ../config/client/.commons | create_non_existing_mounts
     cat ../config/client/.commons | mount_all_shares
-    
+
     host_specific_exportsfile=$(hostname)-$(hostname -I | awk '{ print $1 }')
 
     cat ../config/client/"$host_specific_exportsfile" | create_non_existing_mounts
--- Env_setup_scripts/nfs/server/nfs_serverside_configurer.sh
+++ Env_setup_scripts/nfs/server/nfs_serverside_configurer.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 update_etc_exports() {
-    xargs -I X echo "grep -F \"X\" /etc/exports > /dev/null || echo \"X\" | sudo tee -a /etc/exports" | bash
+    grep -E -v " *#" | xargs -I X echo "grep -F \"X\" /etc/exports > /dev/null || echo \"X\" | sudo tee -a /etc/exports" | bash
 }
 
 create_non_existing_shares() {
-    xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "[[ -d X ]] || sudo mkdir -p X" | bash
+    grep -E -v " *#" | xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "[[ -d X ]] || sudo mkdir -p X" | bash
 }
 
 create_backup() {
@@ -14,12 +14,12 @@ create_backup() {
 
 # Needed for PV recycler pod in K8 to have access to clean PV
 none_owner() {
-    xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chown nobody:nogroup X" | bash
+    grep -E -v " *#" | xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chown nobody:nogroup X" | bash
 }
 
 # Needed for PV recycler pod in K8 to have access to clean PV
 everyone_can_modify() {
-    xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chmod 777 X" | bash
+    grep -E -v " *#" | xargs -I X echo X | awk '{ print $1 }' | xargs -I X echo "sudo chmod 777 X" | bash
 }
 
 # https://ubuntu.com/server/docs/service-nfs
@@ -30,7 +30,7 @@ config_server_setup() {
     cat ../config/server/.commons | create_non_existing_shares
     cat ../config/server/.commons | none_owner
     cat ../config/server/.commons | everyone_can_modify
-    
+
     host_specific_exportsfile=$(hostname)-$(hostname -I | awk '{ print $1 }')
 
     cat ../config/server/"$host_specific_exportsfile" | update_etc_exports

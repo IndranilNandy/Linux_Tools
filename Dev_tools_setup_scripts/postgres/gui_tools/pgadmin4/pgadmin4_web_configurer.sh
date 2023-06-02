cdad4b6073e73bc89c3c4fc10a9bd3696f37683e
IndranilNandy <indranilnandy@test-vm2>
2023-06-02 14:01:41 +0530

    Subject: [postgres] Fix pgadmin4-web installation issue
    Change details:
        1. Fix pgadmin4-web installation issue.
    
    Version:
    
    References:
    
    Notes:

--- Dev_tools_setup_scripts/postgres/gui_tools/pgadmin4/pgadmin4_web_configurer.sh
+++ Dev_tools_setup_scripts/postgres/gui_tools/pgadmin4/pgadmin4_web_configurer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 setup_web() {
-    yes | sudo /usr/pgadmin4/bin/setup-web.sh
+    sudo /usr/pgadmin4/bin/setup-web.sh
 }
 
 echo -e "Configuring web setup for pgAdmin4" && setup_web

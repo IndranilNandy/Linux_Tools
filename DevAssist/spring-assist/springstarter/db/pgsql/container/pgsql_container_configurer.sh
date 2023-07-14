198712725f22193c5274840597ba3bbfecdcf687
IndranilNandy <indranilnandy@devbox6.0>
2023-07-14 22:05:58 +0530

    Subject: [springstarter] Update to add more functionalities
    Change details:
        1. Update to add more functionalities.
    
    Version:
    
    References:
    
    Notes:

--- DevAssist/spring-assist/springstarter/db/pgsql/container/pgsql_container_configurer.sh
+++ DevAssist/spring-assist/springstarter/db/pgsql/container/pgsql_container_configurer.sh
@@ -11,25 +11,92 @@ script="$script_path"/docker-compose.yaml
 
 target_path="$(pwd)"/docker/postgresql
 
-copyContainerScript() {
+find_db_container() {
+    env_file=".env"
+    mapping_file="$curDir"/db/pgsql/container/compose.varsmapping
+    svc_key=$(grep -E "your-service-name" "$mapping_file" | grep -v " *#" | awk -F'/' '{print $3}')
+    svc_value=$(grep -E "$svc_key" "$env_file" | grep -v " *#" | awk -F'=' '{print $2}')
 
-    [[ -d "$target_path" ]] || mkdir -p "$target_path"
-    [[ -f "$target_path"/docker-compose.yaml ]] && echo -e "Warning! docker-compose.yaml already exists, hence not copying. You can edit it manually." ||
-        ( cp "$script" "$target_path" && editor -w "$target_path"/docker-compose.yaml )
+    no=$(sudo -S docker ps --filter ancestor=postgres | grep -c "$svc_value");
+    (( no > 1)) && echo -e "WARNING! Multiple postgres containers running under same project [$svc_value]. Run 'docker compose up' manually." && return 1
+    postgres_container=$(sudo -S docker ps --filter ancestor=postgres | grep "$svc_value" | awk '{print $NF}');
+
+    echo "$postgres_container"
+}
+
+configContainer() {
+    # psql postgresql://"$role_admin"@localhost:"$port"/"$db" <'./dbinit_scripts/init.sql'
+    echo -e "[Compose][Postgres] Now configuring containers"
+
+    postgres_container=$(find_db_container) || return 1
+    user=postgres
+    db=postgres
+
+    echo -e "[Compose][Postgres] db container -> $postgres_container"
+    echo -e "[Compose][Postgres] Configuring db >> docker exec -i $postgres_container bash -c \"psql postgresql://$user@localhost:5432/$db -c '\l'\""
+
+    docker exec -i "$postgres_container" bash -c "psql postgresql://$user@localhost:5432/$db -c '\l'"
+    return 0
 }
 
 composeUp() {
+    echo -e "[Compose][Postgres] Running 'docker compose up -d'"
     docker compose -f "$target_path"/docker-compose.yaml up -d
 }
 
+process_vars_mapping() {
+    env_file=$(pwd)/.env
+    mapping_file="$curDir"/db/pgsql/container/compose.varsmapping
+
+    # echo -e "[Compose][Postgres][Mapping] Compose vaiable -> Env variable"
+    # cat "$mapping_file"
+
+    # xargs -I X echo "grep -E X $env_file | cut -d\"=\" -f2-" < "$compose_vars" | bash | xargs -I Y echo hi Y
+
+    # This code is kept for reference, this is the longer version of the next one-liner code
+    # for mapentry in $(cat "$mapping_file"); do
+    #     mapkey=$(echo "$mapentry" | awk -F'/' '{print $2}')
+    #     mapvalue=$(echo "$mapentry" | awk -F'/' '{print $3}')
+
+    #     env_value=$(grep -E "$mapvalue" "$env_file" | grep -v " *#" | cut -d"=" -f2-)
+    #     echo "s/$mapkey/$env_value/"
+    # done
+
+    xargs -I X echo "echo \"s/\$(echo X | awk -F'/' '{print \$2}')/\$(grep -E \$(echo X | awk -F'/' '{print \$3}') "$env_file" | grep -v " \*#" | cut -d"=" -f2-)/\"" < "$mapping_file" | bash
+}
+
+process_template() {
+    file="${1}"
+    echo -e "[Compose][Postgres] Now substituting env variables in $file"
+    sed -i -f <(process_vars_mapping) "$file"
+
+}
+
+copyContainerScript() {
+    echo -e "[Compose][Postgres] Copying compose template and substituting env variables"
+    echo -e "[Compose][Postgres] Target path: $target_path"
+
+    [[ -d "$target_path" ]] || mkdir -p "$target_path"
+    [[ -f "$target_path"/docker-compose.yaml ]] && echo -e "Warning! docker-compose.yaml already exists, hence not copying. You can edit it manually." && return 0
+    [[ ! -f $(pwd)/.env ]] && echo -e ".env file is missing in the root directory. Skipping container setup" && return 1
+
+    cp "$script" "$target_path" && process_template "$target_path"/docker-compose.yaml && editor -w "$target_path"/docker-compose.yaml || return 1
+    echo -e "[Compose][Postgres] Compose file created in target path [$target_path]"
+
+    return 0
+}
+
 upContainer() {
-    copyContainerScript
-    composeUp
+    copyContainerScript || return 1
+    composeUp || return 1
+    return 0
 }
 
 init() {
-    echo "from init -> ""$*"
-    upContainer
+    echo -e "[Compose][Postgres] Initializing containers"
+    upContainer || return 1
+    configContainer || return 1
+    return 0
 }
 
 up() {
---
+++ DevAssist/spring-assist/springstarter/db/pgsql/container/testfn.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+process_vars_mapping() {
+    env_file=".env"
+    mapping_file="compose.varsmapping"
+    # xargs -I X echo "grep -E X $env_file | cut -d\"=\" -f2-" < "$compose_vars" | bash | xargs -I Y echo hi Y
+
+    # This code is kept for reference, this is the longer version of the next one-liner code
+    for mapentry in $(cat "$mapping_file"); do
+        mapkey=$(echo "$mapentry" | awk -F'/' '{print $2}')
+        mapvalue=$(echo "$mapentry" | awk -F'/' '{print $3}')
+        echo "$mapkey"->"$mapvalue"
+        env_value=$(grep -E "$mapvalue" "$env_file" | grep -v " *#" | cut -d"=" -f2-)
+        echo "1s/$mapkey/$env_value/"
+    done
+
+    # xargs -I X echo "echo \"s/\$(echo X | awk -F'/' '{print \$2}')/\$(grep -E \$(echo X | awk -F'/' '{print \$3}') "$env_file" | grep -v " \*#" | cut -d"=" -f2-)/\"" < "$mapping_file" | bash
+}
+
+process_template() {
+    file="${1}"
+    echo -e "[Compose][Postgres] Now substituting env variables in $file"
+    sed -i -f <(process_vars_mapping) "$file"
+}
+
+find_db_container() {
+    env_file=".env"
+    mapping_file="compose.varsmapping"
+    svc_key=$(grep -E "your-service-name" "$mapping_file" | grep -v " *#" | awk -F'/' '{print $3}')
+    svc_value=$(grep -E "$svc_key" "$env_file" | grep -v " *#" | awk -F'=' '{print $2}')
+
+    no=$(sudo -S docker ps | grep -c "$svc_value");
+    (( no > 1)) && echo -e "WARNING! Multiple postgres containers running under same project [$svc_value]. Run 'docker compose up' manually." && return 1
+    postgres_container=$(sudo -S docker ps --filter ancestor=postgres | grep "$svc_value" | awk '{print $NF}');
+
+    echo $postgres_container
+}
+
+test() {
+    c=$(find_db_container) || echo -e failed && return 1
+    echo "$c"
+    echo abc
+}
+
+test1() {
+    target="/tmp/springstarter"
+    mkdir -p "$target"
+    script=init.sqltemplate
+    process_vars_mapping
+    # cp "$script" "$target"/init.sql && process_template "$target"/init.sql
+    # cat "$target"/init.sql
+}
+
+# process_vars_mapping
+# find_db_container
+test1
